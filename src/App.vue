311c0c0bf9a2fda11b45f995f72dcb81c2f4b49b
guoshuohui <guoshuohui@163.com>
2020-12-18 10:49:53 +0800

    demo123

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home123</router-link> |
       <router-link to="/about">About123</router-link>
     </div>
     <router-view/>
